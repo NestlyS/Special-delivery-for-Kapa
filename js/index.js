04944721974b2da8fd77b7fdebf6e895d90ccb07
NestlyS <49894653+NestlyS@users.noreply.github.com>
2020-05-31 21:07:20 +0300

    Upload files

---
+++ js/index.js
@@ -0,0 +1,29 @@
+document.body.style.height = screen.height + 'px';
+
+let buttons = document.querySelectorAll('[data-button="select"]');
+let blocks = document.querySelectorAll('.block');
+
+
+let audio = new Audio();
+audio.src = '../sound/click.ogg';
+audio.volume = 0.15;
+
+function foo(button, block) {
+    button.addEventListener('click', () => {
+        buttons.forEach((item) => {
+            item.classList.remove('element--selected');
+        })
+        button.classList.add('element--selected');
+        audio.play();
+        if (block !== undefined) {
+            blocks.forEach((item) => {
+                item.classList.remove('block--showed-up');
+            })
+            block.classList.add('block--showed-up');
+        }
+    });
+};
+
+buttons.forEach((item, index) => {
+    foo(item, document.querySelector('.' + item.dataset.block));
+});
